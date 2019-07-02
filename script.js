99f1faad335b2517bb9c58db218ab8e0d54129c9
apc <alexcurran.va@gmail.com>
2019-07-02 10:35:45 -0500

    Finished modal functionality

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-const nav = document.querySelector(".nav");
-const burgerBtn = document.querySelector(".header-burger");
+const navBarModule = (() => {
+    const navItems = document.querySelectorAll(".nav-item");
+    const burgerBtn = document.querySelector(".header-burger");
 
-burgerBtn.addEventListener("click", function dropDown() {
-    nav.classList.toggle("active");
-});
+    burgerBtn.addEventListener("click", function dropDown() {
+        navItems.forEach((li) => {
+            li.classList.toggle("active");
+        });
+    });
+})();
+
+const modalModule = (() => {
+    const subscribeBtn = document.querySelector(".subscribe-btn");
+    const modal = document.querySelector(".modal");
+    const modalBg = document.querySelector(".bg-darken");
+    const modalClose = document.querySelector(".modal-close");
+
+    subscribeBtn.addEventListener("click", () => {
+        modalBg.classList.add("bg-active");
+    });
+
+    modalClose.addEventListener("click", () => {
+        modalBg.classList.remove("bg-active");
+    });
+
+    modalBg.addEventListener("click", (event) => {
+        if (event.target === modalBg) {
+            modalBg.classList.remove("bg-active");
+        }
+    });
+})();
